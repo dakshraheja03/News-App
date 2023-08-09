25f027147bc34da089c2efad5f32c13c4c63f0a6
Daksh Raheja <dakshraheja03@gmail.com>
2023-08-10 00:43:55 +0530

    dark mode

--- src/components/News.js
+++ src/components/News.js
@@ -3,19 +3,30 @@ import NewsItem from './NewsItem'
 
 export default function News() {
     const [data, setdata] = useState([])
+    const [page, setpage] = useState(1)
     const getApiData = async () => {
         const response = await fetch(
-          "https://newsapi.org/v2/top-headlines?country=us&apiKey=39cda8a8d8ba46e49b3d796b9ea90348"
+          `https://newsapi.org/v2/top-headlines?country=us&apiKey=b9bc9141a1ef4e2aa6e74cbe80cfb9fc&page=${page}&pageSize=18`
         ).then((response) => response.json());
         setdata(response);
       };
       useEffect(() => {
-        getApiData();
-        }, [])
+        getApiData(); // eslint-disable-next-line
+        },[page])
+    
+        const handlePrev=async ()=>{
+            console.log("prev")
+            setpage(page-1)
+            }
+        const handleNext=async ()=>{
+        console.log("next")
+        setpage(page+1)
+            }
+            document.body.style.backgroundColor="black"
   return (
     <>
-    <h1 className='my-2 mx-2'>News- Top Headlines</h1>
-        <div className="container text-center">
+    <h1 className='my-4 mx-2 text-center text-white'>News- Top Headlines</h1>
+        <div className="container text-center mx-5">
             <div className="row">
             {data.articles && data.articles.map((element)=>{
                 return <div className="col-md-4 my-4" key={element.url} >
@@ -23,6 +34,10 @@ export default function News() {
                         </div>
             })}
             </div>
+        </div>
+        <div className="d-flex justify-content-between">
+        <button disabled={page<=1} type="button" className="btn btn-dark" onClick={handlePrev}>&larr; Prev</button>
+        <button disabled={page+1 >Math.ceil(data.totalResults/20)} type="button" className="btn btn-dark" onClick={handleNext}>Next &rarr;</button>
         </div>
     </>
   )
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -3,12 +3,12 @@ import React from 'react'
 export default function NewsItem(props) {
   return (
     <>
-    <div className="card" style={{width: "18rem"}}>
+    <div className="card" style={{width: "18rem",backgroundColor:"#404040",border:"1px white solid"}}>
         <img src={props.url?props.url:"https://www.washingtonpost.com/wp-apps/imrs.php?src=https://arc-anglerfish-washpost-prod-washpost.s3.amazonaws.com/public/6RDG4JS6XNJYP62ICIWA44NIWA_size-normalized.jpg&w=1440"} className="card-img-top" alt="..."/>
         <div className="card-body">
-            <h5 className="card-title">{props.title?props.title.slice(0,45):""}...</h5>
-            <p className="card-text">{props.context?props.context.slice(0,88):""}...</p>
-            <a href={props.newsUrl} target='_blank' rel="noreferrer" className="btn btn-primary btn-sm">Go somewhere</a>
+            <h5 className="card-title text-white">{props.title?props.title.slice(0,45):""}...</h5>
+            <p className="card-text text-white">{props.context?props.context.slice(0,88):""}...</p>
+            <a href={props.newsUrl} target='_blank' rel="noreferrer" className="btn btn-dark btn-sm">Go somewhere</a>
         </div>
     </div>
     </>
