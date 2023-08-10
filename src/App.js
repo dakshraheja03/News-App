b332fe2f1021ac6553e80f1d4dc4e6e52a8a3698
Daksh Raheja <dakshraheja03@gmail.com>
2023-08-10 20:05:44 +0530

    added a top loading bar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import News from './components/News';
-import React from 'react';
+import React,{useState} from 'react';
 import { BrowserRouter as Router,Routes, Route} from 'react-router-dom';
+import LoadingBar from 'react-top-loading-bar'
 
 function App() {
+  const [progress,setProgress] = useState(10);
+  const progressSet=(value)=>{
+    setProgress(value)
+  }
   return (
     <>
     <Router>
     <Navbar/>
+      <div>
+      <LoadingBar color="#FFFFFF" progress={progress} />
+      </div>
     <div className="container">
             <Routes>
-                <Route exact path='/' element={<News pageSize={6} category='general'/>}></Route>
-                <Route exact path='/business' element={<News pageSize={6} category='business'/>}></Route> 
-                <Route exact path='/entertainment' element={<News pageSize={6} category='entertainment'/>}></Route> 
-                <Route exact path='/health' element={<News pageSize={6} category='health'/>}></Route> 
-                <Route exact path='/science' element={<News pageSize={6} category='science'/>}></Route> 
-                <Route exact path='/sports' element={<News pageSize={6} category='sports'/>}></Route> 
-                <Route exact path='/technology' element={<News pageSize={6} category='technology'/>}></Route> 
+                <Route exact path='/' element={<News progressSet={progressSet} pageSize={8} category='general'/>}></Route>
+                <Route exact path='/business' element={<News progressSet={progressSet} pageSize={8} category='business'/>}></Route> 
+                <Route exact path='/entertainment' element={<News progressSet={progressSet} pageSize={8} category='entertainment'/>}></Route> 
+                <Route exact path='/health' element={<News progressSet={progressSet} pageSize={8} category='health'/>}></Route> 
+                <Route exact path='/science' element={<News progressSet={progressSet} pageSize={8} category='science'/>}></Route> 
+                <Route exact path='/sports' element={<News progressSet={progressSet} pageSize={8} category='sports'/>}></Route> 
+                <Route exact path='/technology' element={<News progressSet={progressSet} pageSize={8} category='technology'/>}></Route> 
             </Routes>
     </div>
     </Router>
--- src/components/News.js
+++ src/components/News.js
@@ -7,12 +7,15 @@ export default function News(props) {
     const [page, setpage] = useState(1)
     const [load, setload] = useState(false)
     const getApiData = async () => {
+        props.progressSet(10)
         setload(true)
         const response = await fetch(
-          `https://newsapi.org/v2/top-headlines?country=in&apiKey=f79f876e7c3d4af78edfed63a80df42b&category=${props.category}&page=${page}&pageSize=${props.pageSize}`
+          `https://newsapi.org/v2/top-headlines?country=in&apiKey=2307b3a632e744e3bbe174e17ed4965c&category=${props.category}&page=${page}&pageSize=${props.pageSize}`
         ).then((response) => response.json());
+        props.progressSet(50)
         setdata(response);
         setload(false)
+        props.progressSet(100)
       };
       useEffect(() => {
         window.scrollTo(0, 0)
@@ -29,8 +32,8 @@ export default function News(props) {
             document.body.style.backgroundColor="black"
   return (
     <>
-    <h1 className='my-4 text-center text-white'>News- Top Headlines</h1>
-    <div className='text-center' style={{height:"30px",marginTop:"15px"}}>
+    <h1 className='text-center text-white' style={{marginTop:'70px'}}>News- Top Headlines</h1>
+    <div className='text-center' style={{height:"15px",marginTop:"15px"}}>
             {load && <Spinner/>}
     </div>
         <div className="container text-center">
@@ -42,7 +45,7 @@ export default function News(props) {
             })}
             </div>
         </div>
-        <div className="d-flex justify-content-between">
+        <div className="d-flex justify-content-between my-4">
         <button disabled={page<=1} type="button" className="btn btn-dark" onClick={handlePrev}>&larr; Prev</button>
         <button disabled={page+1 >Math.ceil(data.totalResults/20)} type="button" className="btn btn-dark" onClick={handleNext}>Next &rarr;</button>
         </div>
